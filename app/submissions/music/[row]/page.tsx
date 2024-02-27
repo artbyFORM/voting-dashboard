19180f4dcda479a2115f84f32638d3cec425331f
Erik Martin <techgeek1129@gmail.com>
2024-02-27 11:58:10 -0800

    fetch function to get sheet data

--- app/submissions/music/[row]/page.tsx
+++ app/submissions/music/[row]/page.tsx
@@ -1,28 +1,15 @@
-import { google } from 'googleapis';
+import fetchMusicSubmissionData from '@/app/api/submissions/music/fetchMusicSubmissionData';
 
-export default async function MusicSubmission({params} : {params: {row: string}}) {
-
-    const auth = await google.auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    });
-
-    const sheets = google.sheets({version: 'v4', auth});
-
-    const row = parseInt(params.row) + 1; // +1 is because the first row is the header
-    const range = `A${row}:C${row}`; //FIXME: this is hardcoded for now, needs to be dynamic to any amount of voters
-
-    const response = await sheets.spreadsheets.values.get({
-        spreadsheetId: process.env.SHEET_ID,
-        range
-    });
-
-    const [title, artist, person1] = response.data.values?.[0] ?? []; //FIXME: hardcoded, needs to parse all columns eventually
+export default async function Submission({params} : {params: {row: string}}) {
+   
+    const submissionData = await fetchMusicSubmissionData(params.row);
+    const { title, artist, votes } = submissionData;
 
     return (
         <div>
-            <h1>{title}</h1>
-            <h1>{artist}</h1>
-            <p>{person1}</p>
+            <h1>{`Title: ${title}`}</h1>
+            <h1>{`Artist: ${artist}`}</h1>
+            {votes.map((vote: any, index: number) => (<p key={index}>{vote + "\n"}</p>))}
         </div>
     )
 }
