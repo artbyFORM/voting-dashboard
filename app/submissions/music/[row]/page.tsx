0c120fd684bb4a62b64527251736dd1a13ee041b
Erik Martin <techgeek1129@gmail.com>
2024-02-29 15:12:19 -0800

    style & refactor MORE

--- app/submissions/music/[row]/page.tsx
+++ app/submissions/music/[row]/page.tsx
@@ -55,20 +55,27 @@ export default function Submission({ params }: { params: { row: string } }) {
     };
 
     return (
-        <div className="flex justify-between items-center p-15 w-full h-full">
-            <Link className="btn" href={`/submissions/music/${parseInt(row) - 1}`}>BACK</Link>
-            <div className="flex flex-col items-center p-15 space-y-15">
-                {data ? <h1 className="text-4xl font-extrabold pb-5">{data.title}</h1> : "..."}
-                {data ? <h1 className="text-4xl font-light pb-5">{data.artists}</h1> : "..."}
-                {/*data ? data.votes.map((vote: any, index: number) => (<p key={index}>{vote + '\n'}</p>)): "..."*/}
 
-                <div className="flex">
-                    <button className={`btn text-4xl size-24 px-5 mr-10 ${vote === '1' ? 'btn-primary' : 'btn-red'}`} onClick={() => handleSubmit('1')}>1</button>
-                    <button className={`btn text-4xl size-24 px-5 mr-10 ${vote === '2' ? 'btn-primary' : 'btn-red'}`} onClick={() => handleSubmit('2')}>2</button>
-                    <button className={`btn text-4xl size-24 px-5 mr-10 ${vote === '3' ? 'btn-primary' : 'btn-red'}`} onClick={() => handleSubmit('3')}>3</button>
+        
+        <div>
+            <div className="flex justify-between items-center p-15 w-full h-full">
+                <Link className="btn ml-10" href={`/submissions/music/${parseInt(row) - 1}`}>BACK</Link>
+                <Link className="btn mr-10" href={`/submissions/music/${parseInt(row) + 1}`}>NEXT</Link>
+            </div>
+
+            <div className="flex justify-center items-center p-15 w-full h-full">
+                <div className="flex flex-col items-center p-15 space-y-15">
+                    {data ? <h1 className="text-4xl font-extrabold pb-5">{data.title}</h1> : "..."}
+                    {data ? <h1 className="text-4xl font-light pb-5">{data.artists}</h1> : "..."}
+                    {/*data ? data.votes.map((vote: any, index: number) => (<p key={index}>{vote + '\n'}</p>)): "..."*/}
+
+                    <div className="flex">
+                        <button className={`btn text-4xl size-24 px-5 mr-10 ${vote === '1' ? 'btn-primary' : 'btn-red'}`} onClick={() => handleSubmit('1')}>1</button>
+                        <button className={`btn text-4xl size-24 px-5 mr-10 ${vote === '2' ? 'btn-primary' : 'btn-red'}`} onClick={() => handleSubmit('2')}>2</button>
+                        <button className={`btn text-4xl size-24 px-5 mr-10 ${vote === '3' ? 'btn-primary' : 'btn-red'}`} onClick={() => handleSubmit('3')}>3</button>
+                    </div>
                 </div>
             </div>
-            <Link className="btn" href={`/submissions/music/${parseInt(row) + 1}`}>NEXT</Link>
         </div>
     );
 }
